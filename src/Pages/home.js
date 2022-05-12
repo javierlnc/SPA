458ccddd28f2ce8d92fca0ad2ed699a248b5666e
javierlnc <javierlnc@icloud.com>
2022-05-12 01:06:08 -0500

    Se creo el primer tipo, home para la pagina y la estructura necesaria en el html

---
+++ src/Pages/home.js
@@ -0,0 +1,14 @@
+const home= () =>{
+    const view = `
+    <div class="characters">
+        <article class="character-item">
+            <a href="">
+                <img src="image" alt="name">
+                <h2>Name</h2>
+            </a>
+         </article>
+    </div>
+    `;
+    return view;
+}
+export default home;
